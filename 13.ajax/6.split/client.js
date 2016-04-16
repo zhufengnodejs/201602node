345ca6060f8cf5e50949c34a17274871fa691a87
zhangfengnodejs <83687401@qq.com>
2016-04-16 17:58:29 +0800

    ajax对象

---
+++ 13.ajax/6.split/client.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.get("http://baijia.baidu.com", function (res) {
+    //console.log(res);
+    var result = '';
+    res.on('data', function (data) {
+        result += data.toString();
+        ;
+    });
+    res.on('end', function () {
+        console.log(result);
+    });
+}).on('error', function (e) {
+    console.log("错误：" + e.message);
+});
