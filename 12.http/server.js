11cbd3bd626bb65b52f6fb11e60540a3389329fd
zhangfengnodejs <83687401@qq.com>
2016-04-16 16:35:37 +0800

    ajax对象

--- 12.http/server.js
+++ 12.http/server.js
@@ -1,17 +1,23 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 http.createServer(function(req,res){
-   if(req.url == '/'){
+    var urlObj = url.parse(req.url,true);
+   if(urlObj.pathname == '/'){
       fs.createReadStream('./login.html').pipe(res);
-   }else if(req.url =='/login'){//因为用户提交表单的时候会把请求体传过来，所以要接收请求体
-       var buffers = [];
+   }else if(urlObj.pathname =='/login'){//因为用户提交表单的时候会把请求体传过来，所以要接收请求体
+       /*var buffers = [];
        req.on('data',function(data){
            buffers.push(data);
        })
        req.on('end',function(data){
            var result = Buffer.concat(buffers);
             res.end(result.toString());
-       })
+       })*/
+
+
+       console.log(urlObj.query.username);
+       res.end(urlObj.query.username);
    }else{
        res.end('404');
    }
