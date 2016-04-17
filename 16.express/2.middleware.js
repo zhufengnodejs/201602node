6ab1fb17ddda379438a5b6a4cbf64714819b2f30
zhangfengnodejs <83687401@qq.com>
2016-04-17 13:10:16 +0800

    express

--- 16.express/2.middleware.js
+++ 16.express/2.middleware.js
@@ -8,23 +8,39 @@ var app = express();
  */
 app.use(function(req,res,next){
     console.log('use');
-    res.setHeader('Content-Type','text/plain;charset=utf-8');
-    //res.end('over');
+  /*  res.setHeader('Content-Type','text/plain;charset=utf-8');*/
+   // res.end('over');
     next();//如果调用表示继续执行下面的路由
 });
+
+/*app.use(function(req,res,next){
+    var urlObj = require('url').parse(req.url,true);
+    req.path = urlObj.pathname;
+    req.query = urlObj.query;
+    res.send = function(){
+
+    }
+    next();
+});*/
 
 //请求分发也叫路由 根据不同的路径 进行不同的处理
 //处理get请求  的 /index 路径
 app.get('/index',function(req,res){
+    console.log(req.path);//路径pathname
+    console.log(req.query);//查询字符串对象
+    console.log(res.send);//发送的方法
     console.log('张婷 index');
-    res.end('张婷');
+    res.sendStatus(200);
+    res.send(200);
+    // first arguments must be a string or buffer
+    //res.end({name:'张婷'});
 });
 
 //处理get请求 的 /index 路径
 // pathname = req.url 减去查询字符串
 // /hello?name=zfpx /home
-app.get('/home',function(req,res){
-    console.log('张婷 用户主页面');
+app.get('/home/:id/:name',function(req,res){
+    console.log(req.params.id,req.params.name,'用户主页面');
     res.end('用户主页面');
 });
 
@@ -35,5 +51,4 @@ app.get('/home',function(req,res){
     res.end('找不到页面');
 });*/
 
-var http = require('http');
-http.createServer(app.start).listen(8080);
+app.listen(8080);
--- 16.express/express.js
+++ 16.express/express.js
@@ -5,9 +5,13 @@ function express(){
     }
     app.fns = [];
     //配置中间件
-    app.use = function(fn){
+    app.use = function(path,fn){
+        if(typeof path == 'function'){
+            fn = path;
+            path = '/';
+        }
         this.fns.push({
-            path:'/',
+            path:path,
             fn:fn
         });
     }
@@ -20,11 +24,16 @@ function express(){
     }
     app.start = function(req,res){
         var i=0;//当前的层的索引
+        //定义下一个函数
         function next(){
+            //取出每一层
             var layer = app.fns[i++];
+            //判断当前用户请求的路径是否和当前路径相匹配
             if(req.url.indexOf(layer.path)==0){
+                //如果匹配上的话就执行当前的回调函数
                 layer.fn(req,res,next);
             }else{
+                //如果不匹配的话就执行一个匹配
                 next();
             }
         }
