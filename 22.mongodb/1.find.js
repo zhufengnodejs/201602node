be7daffbbaa5a7224bb0004cce2599350d843013
zhangfengnodejs <83687401@qq.com>
2016-04-23 16:26:34 +0800

    find

--- 22.mongodb/1.find.js
+++ 22.mongodb/1.find.js
@@ -2,14 +2,17 @@ var Collection = function(name){
     this.data = [{age:1},{age:2},{age:3},{age:4}];
     this.name = name;
 };
-
+// process.nextTick  当前队列的尾部 一定会晚上所有的同步方法执行
+// setImmediate setTimeout 放在下一个列队的顶部
 Collection.prototype.find = function(){
     var self  = this;
-    var result = this.data.sort(function(a,b){
-        var key = Object.keys(self.sort)[0];
-        return (a[key] - b[key])*self.sort[key];
-    }).slice(this.skip,this.skip+this.limit);
-    console.log(result);
+    process.nextTick(function(){
+        var result = self.data.sort(function(a,b){
+            var key = Object.keys(self.sort)[0];
+            return (a[key] - b[key])*self.sort[key];
+        }).slice(self.skip,self.skip+self.limit);
+        console.log(result);
+    })
     return this;
 }
 
