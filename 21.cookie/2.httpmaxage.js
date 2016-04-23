d4ae8e1fffe971296537a445670e800fc15411b0
zhangfengnodejs <83687401@qq.com>
2016-04-23 11:28:20 +0800

    cookie

--- 21.cookie/2.httpmaxage.js
+++ 21.cookie/2.httpmaxage.js
@@ -6,8 +6,8 @@ http.createServer(function(req,res){
    var urlObj = url.parse(req.url,true);
    var pathname = urlObj.pathname;
    if(pathname == '/buy'){
-       var ts = new Date(Date.now()+30*1000).toGMTString();
-       res.setHeader('Set-Cookie',"goodsname"+Math.random()+"=phone"+Math.random()+"; Expires="+ts);
+
+       res.setHeader('Set-Cookie',"goodsname"+Math.random()+"=phone"+Math.random()+"; maxAge=20000");
        res.end('write');
    }else if(pathname == '/cart'){
        //读取请求头中的cookie
